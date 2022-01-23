213aa1ab5000ff98cdc18249c2a8614c2d408465
phongsavanh Mongkhonvilay <a@amacmini-451.lan>
2022-01-23 13:21:45 -0600

    My first Java Program

---
+++ src/Phongsavanh_Mongkhonvilay_Project01.java
@@ -0,0 +1,13 @@
+//Title: Phongsavanh_Mongkhonvilay_Project01
+//Version: 01-17-22
+//Copyright: Copyright (c) 2022
+//Author: Phongsavanh E. Mongkhonvilay
+//Company: Fort Hays State University
+//Description: This program will print Hello, World
+//
+public class Phongsavanh_Mongkhonvilay_Project01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("This is my first Java Program");
+    }
+}
